9d0292c63d9ea18d931bbfc1f2854cee503c2cbe
knmcguire <kimberly@bitcraze.io>
2023-05-15 15:36:02 +0200

    add crazyflie urdf and edit driver

--- webots_ros2_crazyflie/webots_ros2_crazyflie/crazyflie_driver.py
+++ webots_ros2_crazyflie/webots_ros2_crazyflie/crazyflie_driver.py
@@ -1,6 +1,32 @@
-def main():
-    print('Hi from webots_ros2_crazyflie.')
+# Copyright 1996-2023 Cyberbotics Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""ROS 2 Crazyflie driver."""
+
+import rclpy
 
 
-if __name__ == '__main__':
-    main()
+class CrazyflieDriver:
+    def init(self, webots_node, properties):
+        self.robot = webots_node.robot
+        self.timestep = int(self.__robot.getBasicTimeStep())
+
+        # ROS interface
+        rclpy.init(args=None)
+        self.node = rclpy.create_node('crazyflie_driver')
+
+
+    def step(self):
+        rclpy.spin_once(self.node, timeout_sec=0)
+
