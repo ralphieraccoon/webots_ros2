41510c3c068ddea366ebab76596e8af9f52516ba
knmcguire <kimberly@bitcraze.io>
2023-05-15 15:12:57 +0200

    add info and licences

--- webots_ros2_crazyflie/setup.py
+++ webots_ros2_crazyflie/setup.py
@@ -4,19 +4,21 @@ package_name = 'webots_ros2_crazyflie'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='2023.0.3',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
-    install_requires=['setuptools'],
+    install_requires=['setuptools', 'launch'],
     zip_safe=True,
-    maintainer='kimberly',
+    author='Kimberly McGuire',
+    author_email='kimberly@bitcraze.io',
+    maintainer='Kimberly McGuire',
     maintainer_email='kimberly@bitcraze.io',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    description='The Webots ROS2 package for the Crazyflie',
+    license='Apache License, Version 2.0',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
