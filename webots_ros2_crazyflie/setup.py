a7a0be2230f70b513664116cb5ef0d49387d4093
knmcguire <kimberly@bitcraze.io>
2023-05-15 16:24:59 +0200

    added world to crazyflie package

--- webots_ros2_crazyflie/setup.py
+++ webots_ros2_crazyflie/setup.py
@@ -1,16 +1,21 @@
 from setuptools import setup
 
 package_name = 'webots_ros2_crazyflie'
+data_files = []
+data_files.append(('share/ament_index/resource_index/packages', ['resource/' + package_name]))
+data_files.append(('share/' + package_name, ['package.xml']))
+data_files.append(('share/' + package_name + '/worlds', [
+    'worlds/crazyflie_simple_world.wbt', 'worlds/.crazyflie_simple_world.wbproj',
+]))
+data_files.append(('share/' + package_name + '/resource', [
+    'resource/crazyflie_webots.urdf'
+]))
 
 setup(
     name=package_name,
     version='2023.0.3',
     packages=[package_name],
-    data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-    ],
+    data_files=data_files,
     install_requires=['setuptools', 'launch'],
     zip_safe=True,
     author='Kimberly McGuire',
